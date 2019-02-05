1ad965a88d03edadb9f80f2685be5fcbcf64cf82
nakagawatenma <tenko1177@gmail.com>
2019-02-05 15:53:10 +0900

    add player

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 class Player{
 	constructor(){
 		this.id = Math.floor(Math.random()*1000000000);
@@ -5,12 +6,8 @@ class Player{
 		this.y = 0;
 		this.z = 0;
 	}
-
-	// 移動処理
-	move(distance){
-
-	}
 };
+
 
 let players = {};
 
@@ -22,15 +19,39 @@ const wss = new WebSocketServer({
 
 wss.on('connection',function(ws) {
 	let player = null;
-	// 受信時
+	player = new Player();
+	players[player.id] = player;
+
+
+	// 入室
+	wss.clients.forEach(function(client){
+		client.send(JSON.stringify(player));
+	});
+
+		// 受信時
 		ws.on('message',function(message){
 		console.log('received: %s', message);
-		// プレイヤーの作成
-		player = new Player();
-		players[player.id] = player;
+		var json = JSON.parse(message);
+		
+		if(!players[player.id]){
+			// Idの中身がない時強制終了
+			return;
+		}
+
+		//Json作成
+		//JSON.stringify
+		//Json分解
+		//JSON.parse
+
+		// サーバー側でのデータ管理用		
+		players[json.id].x = json.position.x;
+		players[json.id].y = json.position.y;
+		players[json.id].z = json.position.z;
 
 		wss.clients.forEach(function(client){
-			client.send(message);
+			client.send(JSON.stringify(players[player.id]));
 		});
+
+
 	});
 });
