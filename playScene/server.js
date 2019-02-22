cd457be3c2e570fd0bbb7176292e45e6864f07cc
nakagawatenma <tenko1177@gmail.com>
2019-02-22 09:44:33 +0900

    バグ修正

--- playScene/server.js
+++ playScene/server.js
@@ -1,6 +1,11 @@
-
-// プレイヤークラス
-var playerScript = require("player.js");
+class Player{
+	constructor(){
+		this.id = Math.floor(Math.random()*1000000000);
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+	}
+};
 
 
 let players = {};
